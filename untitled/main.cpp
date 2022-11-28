0647c5a1373db1e801fad69d7b1935f30cc51c12
n s <a@a.a>
2022-11-28 17:30:17 +0545

    integrate UI

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <string>
-#include <cpr/cpr.h>
-#include "converter.h"
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QFile>
 
 int main(int argc, char** argv) {
-    std::string KEY = "7f901f5db2084e6d9f2fdbe72ec3b551";
-    std::string URL = "https://api.currencyfreaks.com/latest?apikey=";
-    URL+=KEY;
-    URL+="&format=json";
 
-    cpr::Response r = cpr::Get(cpr::Url{URL});
-   std::cout << "Status code: " << r.status_code << '\n';
-    std::string response = r.text;
+    QApplication a(argc,argv);
+    MainWindow w;
+    w.setWindowTitle("Currency bois");
+    w.show();
 
-    map<string,double> conversionRates;
-    conversionRates = parseString(response);
-    giveConversion("INR","AUD",100.000,conversionRates);
+    return a.exec();
 }
--- untitled/mainwindow.cpp
+++ untitled/mainwindow.cpp
@@ -1,15 +1,53 @@
-#include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "mainwindow.h"
+
+std::vector<std::string> Codes = {"AUD","INR","NPR","CAD","CNY"};
+
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    for(auto e:Codes){
+        ui->comboBox->addItem(QString::fromLocal8Bit(e));
+        ui->comboBox_2->addItem(QString::fromLocal8Bit(e));
+    }
+    ui->Status->setText("No Conversion Done!!");
+    makeConnection();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::makeConnection(){
+    std::string KEY = "7f901f5db2084e6d9f2fdbe72ec3b551";
+    std::string URL = "https://api.currencyfreaks.com/latest?apikey=";
+    URL+=KEY;
+    URL+="&format=json";
+    cpr::Response r = cpr::Get(cpr::Url{URL});
+    std::cout << "Status code: " << r.status_code << '\n';
+    std::string response = r.text;
+    std::cout<<response<<endl;
+    this->conversionRates = parseString(response);
+}
+
+void MainWindow::on_pushButton_clicked(){
+    QString inCurr = ui->comboBox->currentText();
+    QString outCurr = ui->comboBox_2->currentText();
+    QString amount = ui->input->displayText();
+
+    double result = giveConversion(inCurr.toLocal8Bit().constData(),outCurr.toLocal8Bit().constData(),amount.toDouble(),conversionRates);
+    std::string s_result = std::to_string(result);
+    std::cout<<s_result<<std::endl;
+    ui->output->setText(QString::fromLocal8Bit(s_result));
+    ui->Status->setText("Conversion Success!!!");
+}
+void MainWindow::on_pushButton_2_clicked(){
+    close();
 }
 
